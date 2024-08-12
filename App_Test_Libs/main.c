b695e0fc40a2461566d746a23f325b93cc1e66b8
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2024-08-12 16:01:31 -0300

    Terminada la app de librerias. V1.0.0

--- App_Test_Libs/main.c
+++ App_Test_Libs/main.c
@@ -1,3 +1,15 @@
+/**
+    Programa se utiliza como testeo de algunas librerias realizados en Lenguaje C.
+    Las mismas están en el repositorio de GitHub (https://github.com/FedeManzano/c-libs).
+    Incluido.
+
+    Fechas, Algoritmos de Ordenamiento, ABB, Lista Doble, Cola, Pila.
+
+    @author Federico Manzano (https://github.com/FedeManzano)
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "./init_app/init_app.h"
